c368ebf8bc723252fb73ea2fdcf188c8b0890f22
Som Ramnani <som.ramnani@gmail.com>
2019-03-12 19:46:09 -0500

    Fixed the random number for each buttons

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -9,92 +9,113 @@ var losses = 0;
 //Random Number
 var random = Math.floor(Math.random() * 120) + 19;
 
-// var crystalRandomNum = Math.floor(Math.random() * 1) + 8;
+//Generate random number for each crystal
+var num1 = Math.floor(Math.random()*12+1);
+var num2 = Math.floor(Math.random()*12+1);
+var num3 = Math.floor(Math.random()*12+1);
+var num4 = Math.floor(Math.random()*12+1);
+
+//Score Variable
 var score = 0;
 
 //__________________________________________________________
-//GAME FUNCTION
-//__________________________________________________________
+
 $(document).ready(function() {
-
-
 	
-	var random2 =  Math.floor(Math.random() *10 ) +1;
-	var newP = $("<p>");
-
-	// Displays the random number in the div with the ID = computerscore-text
+	// Displays the random number in the div with the ID = computerscore.
 	$("#computerscore").text(random);
 
-
-	var numberOptions = [10, 5, 3, 7];
-	var crystalButtons = $(".crystalbtn");
-
-	// for (var i = 0; i < crystalButtons.length; i++){
-	// 	var crystalButtonValue  = this.value;
-	// 	var value = crystalButtonValue[i];
-	// 	value = Math.floor(Math.random() * 1) + 8;
-
-	// 	console.log(crystalButtons);
+		//__________________________________________________________
+		// RESET FUNCTION
+		//__________________________________________________________ 
 		
-	// }
-	
-	
-	$(".crystalbtn").on("click", function(){
-			var crystalValue = 		parseInt(this.value) ;
+		function reset(){
+			// Reset the Score
+  		score = 0;
+    	$("#score").html(score);
+  
+     //Reset the random Number
+     	random = Math.floor(Math.random() * 120) + 19;
+			$("#computerscore").text(random);
 			
-			// function reset(score,random){
-			//   //Reset the Score
-   //   score = 0;
-   //   $("#score").html(score);
-  
-   //   //Reset the random Number
-   //   random = Math.floor(Math.random() * 120) + 19;
-   //    $("#computerscore").text(random);
-	
-			// }
-	
-	
-
-		score+= crystalValue;
-		console.log("Crystal 1: "+ crystalValue);
-		console.log(score);
-		$("#score").html(score);
-
-		if (score === random) {
-			wins++;
-   $("#wins").html(wins);
-
-
-
-   //Reset the Score
-   score = 0;
-   $("#score").html(score);
-
-   //Reset the random Number
-   random = Math.floor(Math.random() * 120) + 19;
-    $("#computerscore").text(random);
-
+			//Reset the random Numbers
+			 num1 = Math.floor(Math.random()*12+1);
+			 num2 = Math.floor(Math.random()*12+1);
+			 num3 = Math.floor(Math.random()*12+1);
+			 num4 = Math.floor(Math.random()*12+1);
 		}
-	
-		else if (score >= random) {
-			losses++;
-   $("#losses").text(losses);
-   
-    //Reset the Score
-    score = 0;
-    $("#score").html(score);
- 
-    //Reset the random Number
-    random = Math.floor(Math.random() * 120) + 19;
-     $("#computerscore").text(random);
-  }
-  
-		
-	
-		
-	
+		//__________________________________________________________
 
+
+		//__________________________________________________________
+		//CONDITIONAL STATEMENT FUNCTION
+		//__________________________________________________________
+
+		// A function with the Conditional Statements for the Wins & Losses.
+			function winVsLose(){
+				//If score is correct , you WIN.
+				if (score === random) {
+					wins++;
+			  	$("#wins").html(wins);
+					
+					//Calls the Reset Function
+					reset();
+		 		}
+		 
+		 		// If score is goes above the target number , you LOSE.
+		 		else if (score >= random) {
+					losses++;
+					$("#losses").text(losses);
+					
+					//Calls the Reset Function
+					reset();
+				}
+			}
+		//__________________________________________________________
+
+
+		//__________________________________________________________
+		//BUTTON FUNCTIONS
+		//__________________________________________________________
+
+		//CRYSTAL BUTTON 1
+		$("#crystal1").on("click", function(){
+			score += num1;
+			$("#score").html(score);
+			winVsLose()
+
+			console.log(score);
+			console.log("Crystal 1 Button is:" + num1);
+		});
+
+		//CRYSTAL BUTTON 2
+		$("#crystal2").on("click", function(){
+			score += num2;
+			$("#score").html(score);
+			winVsLose();
+
+			console.log("Crystal 2 Button is:" + num2);
+		});
+
+		//CRYSTAL BUTTON 3
+		$("#crystal3").on("click", function(){
+			score += num3;
+			$("#score").html(score);
+			winVsLose();
+
+			console.log("Crystal 3 Button is:" + num3);
+		});
+
+		//CRYSTAL BUTTON 4
+		$("#crystal4").on("click", function(){
+			score += num4;
+			$("#score").html(score);
+			winVsLose();
+			
+			console.log("Crystal 4 Button is:" + num4);
+		});  		
+		//__________________________________________________________
+	
 	});
-
-});
+	
 
